493b0e21f9ee7d6e8d390c47c46dad84186ccf60
Michael Loschiavo <losch103@gmail.com>
2017-01-22 13:45:21 -0800

    Complete unique chars. O(n) time, O(n) space

--- DataAlgo_forPy/Arrays/unique_chars.js
+++ DataAlgo_forPy/Arrays/unique_chars.js
@@ -1,3 +1,22 @@
-// Given a string,determine if it is compreised of all unique characters.
+// Given a string, determine if it is compreised of all unique characters.
 // For example, the string 'abcde' has all unique characters and should return True.
 // The string 'aabcde' contains duplicate characters and should return false.
+
+
+const unique_chars = (string) => {
+  const char_set = new Set()
+
+  for (var i = 0; i < string.length; i++) {
+    if (!char_set.has(string[i])) {
+      char_set.add(string[i])
+    } else {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(unique_chars('abcde')) // true
+console.log(unique_chars('aabcde')) // false
+console.log(unique_chars('abcdefa')) // false
--- DataAlgo_forPy/Arrays/unique_chars.rb
+++ DataAlgo_forPy/Arrays/unique_chars.rb
@@ -1,3 +1,24 @@
-# Given a string,determine if it is compreised of all unique characters.
+# Given a string, determine if it is compreised of all unique characters.
 # For example, the string 'abcde' has all unique characters and should return True.
 # The string 'aabcde' contains duplicate characters and should return false.
+
+require 'set'
+
+def unique_chars(string)
+  char_set = Set.new()
+
+  string.each_char do |chr|
+    unless char_set.include?(chr)
+      char_set.add(chr)
+    else
+      return false
+    end
+  end
+
+  true
+
+end
+
+p unique_chars('abcde') # true
+p unique_chars('aabcde') # false
+p unique_chars('abcdefa') # false
