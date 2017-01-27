482b818279148f7902c3f79ed1b45d7efeb878fa
Michael Loschiavo <losch103@gmail.com>
2017-01-26 18:33:34 -0800

    complete JS challenges. Both require refactoring

--- DataAlgo_forPy/Recursion/fibonacci_squence.js
+++ DataAlgo_forPy/Recursion/fibonacci_squence.js
@@ -6,3 +6,59 @@
 
 // Function Output:
 // Your function will accept a number n and return the nth number of the fibonacci sequence
+
+const recursive_fib = (number) => {
+  if (number <= 1) {
+    return 1
+  } else if (number === 2) {
+    return 1
+  }
+
+  return recursive_fib(number - 1) + recursive_fib(number - 2)
+}
+
+// console.log(recursive_fib(10)) // 55
+
+
+const iterative_fib = (number) => {
+  let fib_sum = 0
+  let prev = 0
+  let next = 1
+
+  while (number >= 0) {
+    fib_sum += next
+    prev = next
+    next = fib_sum + prev
+    number -= 2
+  }
+
+  return fib_sum
+}
+
+// console.log(iterative_fib(10)) // 55
+
+const dynamic_fib = (number) => {
+  const cache = {}
+  let fib_sum = 0
+  let prev = 0
+  let next = 1
+
+  while (number !== 0) {
+    if (cache.hasOwnProperty(prev)) {
+      let oldValue = prev
+      prev = fib_sum
+      fib_sum += cache[oldValue]
+      next = fib_sum + prev
+    } else {
+      fib_sum += next
+      prev = next
+      next = fib_sum + prev
+      cache[next] = next
+    }
+
+    number -= 2
+  }
+  return next
+}
+
+console.log(dynamic_fib(10)) // 55
--- DataAlgo_forPy/Recursion/string_permutation.js
+++ DataAlgo_forPy/Recursion/string_permutation.js
@@ -4,3 +4,36 @@
 
 // Note: If a character is repeated, treat each occurence as distinct,
 // for example an input of 'xxx' would return a list with 6 "versions" of 'xxx'
+
+const permutations = (string) => {
+  let output = []
+
+  if (string.length === 1) {
+    output = [string]
+  } else {
+
+    for (let i = 0, letter = string[i]; i < string.length; i++) {
+      // Slice from the start of the string upto the current index (left)
+      // Concat first slice with the remaining letters after the index (right)
+      const left = string.substring(0, i)
+      const right = string.substring(i + 1)
+      const permString = left + right
+
+      // Recursively create permutations
+      const single_permutation = permutations(permString)
+
+      for (let j = 0, perm_letter = single_permutation[j]; j < single_permutation.length; j++) {
+        // Loop through permutations and concat each current letter (from outer scope)
+          // with the current permutation set
+        // push string combination to output
+
+        // NOTE: Must concat because our base-case returns an array
+        output.push(letter + perm_letter)
+      }
+    }
+  }
+
+  return output
+}
+
+console.log(permutations("abc")) // ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
