e5e05c228556690fe6ebddce4806cd7f9bde95fe
Michael Loschiavo <losch103@gmail.com>
2017-05-29 12:39:48 -0700

    add routing

--- Angular_React_Vue/angular-framework-intro/src/app/app.module.ts
+++ Angular_React_Vue/angular-framework-intro/src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { UsernameComponent } from './username/username.component'
+import { UsernameComponent } from './username/username.component';
+import { UserComponent } from './user/user.component';
+import { InfoComponent } from './info/info.component';
+import { AccountComponent } from './account/account.component';
+import { BlogComponent } from './blog/blog.component'
+
+const appRoutes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'account', component: AccountComponent },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsernameComponent
+    UsernameComponent,
+    UserComponent,
+    InfoComponent,
+    AccountComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
