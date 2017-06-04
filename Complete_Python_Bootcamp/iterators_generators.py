3e3692d559caace6b243fc57f576753a535b951e
Michael Loschiavo <losch103@gmail.com>
2017-06-04 14:10:19 -0700

    complete iterators challenge

--- Complete_Python_Bootcamp/iterators_generators.py
+++ Complete_Python_Bootcamp/iterators_generators.py
@@ -1,42 +1,59 @@
+import random
+
 # Iterators and Generators Homework
 
 # Problem 1:
 # Create a generator that generates the squares of numbers up to some number N.
 
-def gensquares(n):
-    pass
+
+def gen_squares(n):
+    for num in range(n):
+        yield pow(2, num + 1)
+
+
+for num in gen_squares(4):
+    print(num)
 
 
 # Problem 2
-# Create a generator that yields "n" random numbers between a low and high number (that are inputs).
+# Create a generator that yields "n" random numbers between
+# a low and high number (that are inputs).
 # Note: Use the random library. For example:
-
-# import random
 
 # random.randint(1,10)
 
 def rand_num(low, high, n):
-    pass
+    for _num in range(n):
+        yield random.randint(low, high)
 
-# for num in rand_num(1,10,12):
-    # print num
+
+for num in rand_num(1, 10, 12):
+    print(num)
+
 
 # Problem 3:
 # Use the iter() function to convert the string below
 s = 'hello'
 
-#code here
+# code here
+iter(s)
 
 # Problem 4
-# Explain a use case for a generator using a yield statement where you would not want to use a normal function with a return statement.
+# Explain a use case for a generator using a yield statement where you would
+# not want to use a normal function with a return statement.
 
+# Where the method is taking a high input and requires a large temporary buffer
 
 # Extra Credit!
-# Can you explain what gencomp is in the code below? (Note: We never covered this in lecture! You will have to do some googling/Stack Overflowing!)
+# Can you explain what gencomp is in the code below? (Note: We never covered
+# this in lecture! You will have to do some googling/Stack Overflowing!)
 
-# my_list = [1,2,3,4,5]
+my_list = [1, 2, 3, 4, 5]
 
-# gencomp = (item for item in my_list if item > 3)
+gencomp = (item for item in my_list if item > 3)
 
-# for item in gencomp:
-    # print item
+for item in gencomp:
+    print(item)
+
+# This method is a filter function utilizing a generator. The item is
+# returned if the number is greater than 3
